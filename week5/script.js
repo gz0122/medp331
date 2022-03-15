600986b4223d1cd7c10cbd6ff4b47d855effb119
Grace Zhu <gracezhu1583@gmail.com>
2022-03-14 20:42:28 -0400

    updates

---
+++ week5/script.js
@@ -0,0 +1,25 @@
+async function start(){
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    createBreedList(data.message)
+}
+
+start()
+
+function createBreedList(breedList){
+    document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)">
+        <option>Select a breed</option>
+        ${Object.keys(breedList).map(function(breed){
+            return `<option>${breed}</option>`
+        }).join('')}
+        </select>
+    `
+}
+
+async function loadByBreed(breed){
+    if (breed != "Select a breed"){
+        const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
+        const data = await response.json()
+    }
+}
